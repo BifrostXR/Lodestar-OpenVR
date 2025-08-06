638737a64714487f4e89d9d3ba6a3890c118c7b1
BifrostXR <155929461+BifrostXR@users.noreply.github.com>
2025-08-05 23:14:22 -0600

    installer

---
+++ install.bat
@@ -0,0 +1,37 @@
+@echo off
+echo Lodestar Driver Installer
+
+FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam" /v InstallPath`) DO (
+    set "STEAMVR_DIR=%%A %%B\steamapps\common\SteamVR"
+)
+
+set "STEAMVR_DIR=%STEAMVR_DIR:Steam =Steam%"
+
+set "PWD=%cd%"
+set "DEVICE_NAME=lodestar"
+
+set "DRIVER_DEST=%STEAMVR_DIR%\drivers\%DEVICE_NAME%"
+set "DRIVER_SRC=%PWD%\%DEVICE_NAME%"
+
+set "VRPATHREG_PATH=%STEAMVR_DIR%\bin\win64\vrpathreg.exe"
+
+REM Create the destination directory if it doesn't exist
+mkdir "%DRIVER_DEST%" 2>nul
+
+REM Display the contents of the source and destination directories
+echo Source Directory: "%DRIVER_SRC%"
+echo Destination Directory: "%DRIVER_DEST%"
+
+REM Copy the entire folder to the destination
+echo Copying Files...
+xcopy /Q /E /Y "%DRIVER_SRC%" "%DRIVER_DEST%"
+
+rem Check the exit code of xcopy
+if %errorlevel% neq 0 (
+    echo File Copy Failed! Error Code: %errorlevel%
+    echo Please Manually Move the Source to Destination.
+    pause
+    exit /b %errorlevel%
+) else (
+    echo Driver Installed successfully!
+)
---
+++ install_vars.cmd
@@ -0,0 +1,12 @@
+@echo off
+FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam" /v InstallPath`) DO (
+    set "STEAMVR_DIR=%%A %%B\steamapps\common\SteamVR"
+)
+
+set "PWD=%cd%"
+set "DEVICE_NAME=lodestar"
+
+set "DRIVER_DEST=%STEAMVR_DIR%\drivers\%DEVICE_NAME%"
+set "DRIVER_SRC=%PWD%\%DEVICE_NAME%"
+
+set "VRPATHREG_PATH=%STEAMVR_DIR%\bin\win64\vrpathreg.exe"
